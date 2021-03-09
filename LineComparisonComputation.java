5306f5bd64630c2b25a945b81b749f608eef90e8
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-09 23:29:08 +0530

    [Samiksha].ADD compare two line with compare inbuild method

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -11,23 +11,31 @@ public static void main (String args []){
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
         double firstLineLength = Math.pow(x2-x1,2) + Math.pow(y2-y1,2);
-	String length1 = Double.toString(firstLineLength);
-
-        //Inputs for line 2
+	int length1 = (int) firstLineLength;
+//
+	//Inputs for line 2
         System.out.print("Enter x y conrdinate of point 1 and 2 respectively for line 2 : \n");
         int x3 = sc.nextInt();
         int y3 = sc.nextInt();
         int x4 = sc.nextInt();
         int y4 = sc.nextInt();
         double secondLineLength = Math.pow(x4-x3,2) + Math.pow(y4-y3,2);
-	String length2 = Double.toString(secondLineLength);
+	int length2 = (int) secondLineLength;
 
-	if (length1.equals(length2))
-
-              System.out.println("Lines are equal");
-        else
-           System.out.println("Lines are not equal");
-        }
+	//Comparing two line length
+	int compareValue = Integer.compare(length1,length2);
+	switch(compareValue){
+        case 0 :
+                System.out.println("Lines are equal");
+                break;
+        case 1 :
+                System.out.println("Lines 1 is greater than line 2");
+                break;
+        default :
+                System.out.println("Line 1 is less than line 2");
+                break;
+                }
+	}
 }
 
 
